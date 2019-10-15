dfa07491b8b3740bb118b402c4ceec9542fa2bf9
Jason Nguyen <gofightnguyen.dev@outlook.com>
2019-10-15 17:36:52 -0600

    UnitTestingWithPython - 4 Mocks

--- UnitTestingWithPython/4_TestDoubles/single_sign_on.py
+++ UnitTestingWithPython/4_TestDoubles/single_sign_on.py
@@ -15,8 +15,8 @@ class SSOToken:
     def __init__(self):
         self.id = random.randrange(100000)
 
-    def __eq__(self, o):
-        return self.id == o.guid
+    # def __eq__(self, o):
+    #     return self.id == o.guid
 
     def __repr__(self):
         return str(self.id)
