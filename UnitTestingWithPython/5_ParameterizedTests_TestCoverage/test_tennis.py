f89a981505d920f8e3d334b9bb10e33538ab3e28
Jason Nguyen <gofightnguyen.dev@outlook.com>
2019-10-16 08:15:43 -0600

    UnitTestingWithPython - 5 coverage

--- UnitTestingWithPython/5_ParameterizedTests_TestCoverage/test_tennis.py
+++ UnitTestingWithPython/5_ParameterizedTests_TestCoverage/test_tennis.py
@@ -5,10 +5,18 @@ from tennis import score_tennis
 @pytest.mark.parametrize('player1_points, player2_points, expected_score',
     [(0, 0, 'Love-All'),
      (1, 1, 'Fifteen-All'),
-     (2, 2, 'Thirty-All')
+     (2, 2, 'Thirty-All'),
+     (2, 1, 'Thirty-Fifteen'),
+     (3, 1, 'Forty-Fifteen'),
+     (4, 1, 'Win for Player 1'),
+     (4, 3, 'Advantage Player 1'),
+     (3, 4, 'Advantage Player 2') # take this line out to see a branch not covered
      ])
 def test_score_tennis(player1_points, player2_points, expected_score):
     assert score_tennis(player1_points, player2_points) == expected_score
+
+# to get coverage using pytest
+# pytest --cov-report html:directory --cov-branch --cov=tennis .
 
 # example using unittest
 # def test_score_tennis(self):
